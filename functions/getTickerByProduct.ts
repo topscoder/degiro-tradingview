10f2bb84a70f974f23c20d5fc4ba1891b3c80f4f
topscoder <86197446+topscoder@users.noreply.github.com>
2023-07-07 10:10:18 +0200

    Show PNL in leaderboard

--- functions/getTickerByProduct.ts
+++ functions/getTickerByProduct.ts
@@ -1,40 +1,44 @@
 
 /**
  * getTicketByProduct
- * 
+ *
  * @param product (array[symbol:string, vwdId:int])
  * @returns object { tickerlabel:string, ticker:string}
  */
 export default (product: any) => {
-    let tickerlabel = ""
-    let ticker = ""
+  let tickerlabel = ""
+  let ticker = ""
 
-    // Try symbol
-    if ( product['symbol'] != "" ) {
-        tickerlabel = "syminfo.ticker"
-        ticker = product['symbol']
+  // Try symbol
+  if (product['symbol'] != "") {
+    tickerlabel = "syminfo.ticker"
+    ticker = product['symbol']
 
-    } else {
-        // Try vwdId
-        let vwd : Array<any> = product['vwdId'].toString().match(/^(.*?)\.(.*?)\,(.*?)$/)  // "vwdId": "MSFT.NASDAQ,E",
-        if ( vwd != null && vwd.length > 0 ) {
-            tickerlabel = "syminfo.tickerid"
-            ticker = `${vwd[2]}:${vwd[1]}` // NASDAQ:MSFT
-        }
+  } else {
+    // Try vwdId
+    let vwd: Array<any> = product['vwdId'].toString().match(/^(.*?)\.(.*?)\,(.*?)$/)  // "vwdId": "MSFT.NASDAQ,E",
+    if (vwd != null && vwd.length > 0) {
+      tickerlabel = "syminfo.tickerid"
+      ticker = `${vwd[2]}:${vwd[1]}` // NASDAQ:MSFT
     }
+  }
 
-    // TODO: Fix fundamentally
-    if ( ticker == "FB2A" ) {
-        ticker = "META"
-    }
+  // TODO: Fix fundamentally
+  if (ticker == "FB2A") {
+    ticker = "META"
+  }
 
-    if ( ticker == "AEND" ) {
-        ticker = "AGN"
-    }
-    
-    if ( ticker == "116" ) {
-        ticker = "WATT"
-    }
+  if (ticker == "AEND") {
+    ticker = "AGN"
+  }
 
-    return { "tickerlabel": tickerlabel, "ticker": ticker }
+  if (ticker == "116") {
+    ticker = "WATT"
+  }
+
+  if (ticker == "BP.") {
+    ticker = "BP"
+  }
+
+  return { "tickerlabel": tickerlabel, "ticker": ticker }
 }
--- index.ts
+++ index.ts
@@ -13,35 +13,36 @@ let content = "";
 
 (async () => {
 
-    const now = new Date()
+  const now = new Date()
 
-    // TradingView Indicator Header
-    content += "// @version=5 \n"
-    content += `indicator("${PORTO_LABEL}", "", true) \n`
-    content += `// Generated at ${now.toLocaleString()} \n`
-    content += `// Add to Pine Editor, click Add to Chart\n`
-    content += `// Ensure "Indicator and financials name labels" is enabled\n`
-    content += `// AVG = Average share price\n`
-    content += `\n`
+  // TradingView Indicator Header
+  content += "// @version=5 \n"
+  content += `indicator("${PORTO_LABEL}", "", true) \n`
+  content += `// Generated at ${now.toLocaleString()} \n`
+  content += `// Add to Pine Editor, click Add to Chart\n`
+  content += `// Ensure "Indicator and financials name labels" is enabled\n`
+  content += `// AVG = Average share price\n`
+  content += `\n`
 
-    for (let account of accounts) {
-        let obj = await fetchPositionsAndOrders(account)
-        porto_positions.push(...obj.porto_positions)
-        content += obj.content
-    }
+  for (let account of accounts) {
+    let obj = await fetchPositionsAndOrders(account)
+    porto_positions.push(...obj.porto_positions)
+    content += obj.content
+  }
 
-    content += `\n\nimport MA_PT/easytable/1\n`
-    content += `var string json_porto = '[`
-    for (let pos of porto_positions) {
-        content += `{"ticker": "${pos.tickerId}", "value": "${pos.totalValue}_${pos.currency}", "PNL": "${pos.pnlPercentage}%"}, `
-    }
-    content += `]'\n`
-    content += `var tbl = easytable.json_to_table(json_porto)\n`
-    content += `easytable.change_table_style(tbl, 3, ${porto_positions.length+1}, 3)\n`
+  content += `\n\nimport MA_PT/easytable/1\n`
+  content += `var string json_porto = '[`
+  for (let pos of porto_positions) {
+    let posPnl = pos.pnlPercentage > 0 ? "+" + pos.pnlPercentage : pos.pnlPercentage
+    content += `{"ticker": "${pos.tickerId}", "value": "${pos.totalValue}_${pos.currency}", "PNL": "${posPnl}%"}, `
+  }
+  content += `]'\n`
+  content += `var tbl = easytable.json_to_table(json_porto)\n`
+  content += `easytable.change_table_style(tbl, 3, ${porto_positions.length + 1}, 3)\n`
 
-    fs.writeFileSync('./porto.pine', content);
+  fs.writeFileSync('./porto.pine', content);
 
-    console.log('[>] porto.pine written.')
-    console.log('[>] Now open porto.pine in: TradingView > Pine Editor, Click "Add to Chart" and open one of your positions.')
+  console.log('[>] porto.pine written.')
+  console.log('[>] Now open porto.pine in: TradingView > Pine Editor, Click "Add to Chart" and open one of your positions.')
 
 })()
